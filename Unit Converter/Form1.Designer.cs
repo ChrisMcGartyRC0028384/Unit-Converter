d356faf98e3c9d1fcee0d61c9ca73b4d40b7b189
rc0028384 <rc0028384@CRO-ICF07-24>
2018-02-07 13:59:46 +0000

    half completed

--- Unit Converter/Form1.Designer.cs
+++ Unit Converter/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -46,7 +54,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(493, 166);
+            this.comboBox2.Location = new System.Drawing.Point(493, 165);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 1;
@@ -70,11 +78,91 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 192);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(268, 191);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(493, 191);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 7;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(162, 218);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(493, 217);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 9;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(268, 217);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 10;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(418, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(673, 462);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox2);
@@ -92,6 +180,14 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Unit Converter/Form1.cs
+++ Unit Converter/Form1.cs
@@ -12,7 +12,7 @@ namespace Unit_Converter
 {
     public partial class Form1 : Form
     {
-        Dictionary<string, double> prefixes = new Dictionary<string, double>()
+        Dictionary<string, double> lengths = new Dictionary<string, double>()
         {
            {"nm", 1e-9 },
            {"μm", 1e-6 },
@@ -53,13 +53,28 @@ namespace Unit_Converter
         public Form1()
         {
             InitializeComponent();
-            foreach (string k in areas.Keys)
+            foreach (string k in lengths.Keys)
             {
                 comboBox1.Items.Add(k);
                 comboBox2.Items.Add(k);
             }
+
+            foreach (string k in areas.Keys)
+            {
+                comboBox3.Items.Add(k);
+                comboBox4.Items.Add(k);
+            }
+            foreach (string k in volumes.Keys)
+            {
+                comboBox5.Items.Add(k);
+                comboBox6.Items.Add(k);
+            }
             comboBox1.SelectedIndex = 4;
             comboBox2.SelectedIndex = 4;
+            comboBox3.SelectedIndex = 4;
+            comboBox4.SelectedIndex = 4;
+            comboBox5.SelectedIndex = 4;
+            comboBox6.SelectedIndex = 4;
             update();
         }
 
@@ -70,10 +85,18 @@ namespace Unit_Converter
                 double d = double.Parse(textBox1.Text);
                 double d2 = d * areas[comboBox1.Text] / areas[comboBox2.Text];
                 label1.Text = d2.ToString();
+                double d3 = double.Parse(textBox2.Text);
+                double d4 = d3 * areas[comboBox3.Text] / areas[comboBox4.Text];
+                label2.Text = d4.ToString();
+                double d5 = double.Parse(textBox3.Text);
+                double d6 = d5 * volumes[comboBox5.Text] / volumes[comboBox6.Text];
+                label3.Text = d6.ToString();
             }
             catch
             {
                 label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
             }
         }
 
@@ -88,6 +111,36 @@ namespace Unit_Converter
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
             update();
         }
